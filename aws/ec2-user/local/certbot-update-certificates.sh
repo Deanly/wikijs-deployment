9fd1cce3090a666c2ce3a429eb2b7eadbc2e9e88
deanly <deanly88@gmail.com>
2022-12-22 23:30:13 +0900

    fix shall script for crontab to upate certificate.

--- aws/ec2-user/local/certbot-update-certificates.sh
+++ aws/ec2-user/local/certbot-update-certificates.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker run -it --rm --name certbot -v '/home/ec2-user/service/certbot/conf:/etc/letsencrypt' -v '/home/ec2-user/service/certbot/logs:/var/log/letsencrypt' -v '/home/ec2-user/service/certbot/data:/var/www/certbot' certbot/certbot certificates
+docker run -it --rm --name certbot -v '/home/ec2-user/service/certbot/conf:/etc/letsencrypt' -v '/home/ec2-user/service/certbot/logs:/var/log/letsencrypt' -v '/home/ec2-user/service/certbot/data:/var/www/certbot' certbot/certbot renew
+docker exec deploy_web_1 service nginx reload
