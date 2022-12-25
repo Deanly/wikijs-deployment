133d370c379242773862dc0b9fbbd36ba0fbe686
deanly <deanly88@gmail.com>
2022-12-26 02:31:19 +0900

    add certbot option for be running in a terminal.

--- aws/ec2-user/local/certbot-update-certificates.sh
+++ aws/ec2-user/local/certbot-update-certificates.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run -it --rm --name certbot -v '/home/ec2-user/service/certbot/conf:/etc/letsencrypt' -v '/home/ec2-user/service/certbot/logs:/var/log/letsencrypt' -v '/home/ec2-user/service/certbot/data:/var/www/certbot' certbot/certbot renew
+docker run -it --rm --name certbot -v '/home/ec2-user/wiki/certbot/conf:/etc/letsencrypt' -v '/home/ec2-user/wiki/certbot/logs:/var/log/letsencrypt' -v '/home/ec2-user/wiki/certbot/data:/var/www/certbot' certbot/certbot renew --non-interactive
 docker exec deploy_web_1 service nginx reload
